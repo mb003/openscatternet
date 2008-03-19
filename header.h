f609e88188d0d5c2b9521af48166fae83b8b3434
udinesh5 <udinesh5@0824fa5e-5746-0410-bc4d-cd046f43aa82>
2008-03-19 21:15:46 +0000

    Logical mistake cleared, nicer formatting and added constants.

--- header.h
+++ header.h
@@ -2,3 +2,6 @@
 #define FREE_NODE 0
 #define ROOT_NODE 1
 #define NON_ROOT_NODE 2
+#define TOPOLOGY_FILE "topology.txt"
+#define NODE_STATUS_FILE "node_status.conf"
+#define NODE_STATUS_FILE1 "../node_status.conf"
